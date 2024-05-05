8138f41d0885704c125ba8a511cf60c9cff5e8a7
vespucchi <56511717+vespucchi@users.noreply.github.com>
2024-05-05 18:11:07 +0200

    Create few functions to handle operators operateTwoNumbers <- for operators that take two numbers operateOneNumber <- for operators that take one number clear <- to handle clearing the current input/display

--- script.js
+++ script.js
@@ -8,4 +8,47 @@ function power(a) { return a ** 2 };
 function squareRoot(a) { return Math.sqrt(a) };
 function changeOperator(a) { return a !== 0 ? a * (-1) : a};
 
+let firstNumber = 0;
+let operator;
+let secondNumber;
 
+function operateTwoNumbers(firstNumber, operator, secondNumber) {
+    switch (operator) {
+        case "+":
+            return add(firstNumber, secondNumber);
+        case "-":
+            return subtract(firstNumber, secondNumber);
+        case "*":
+            return multiply(firstNumber, secondNumber);
+        case "/":
+            return divide(firstNumber, secondNumber);
+        case "%":
+            return modulo(firstNumber, secondNumber);
+    }
+}
+
+
+function operateOneNumber(firstNumber, operator) {
+    switch (operator) {
+        case "1/x":
+            return oneDividedBy(firstNumber);
+        case "pow":
+            return power(firstNumber);
+        case "sqrt":
+            return squareRoot(firstNumber);
+        case "+-":
+            return changeOperator(firstNumber);
+    }
+} 
+
+
+function clear(operator) {
+    switch (operator) {
+        case "CE":
+            return ;
+        case "C":
+            return ;
+        case "bsp":
+            return ;
+    }
+}
