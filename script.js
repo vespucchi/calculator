56b9cfe197835e77534cd22778dae16b85f30b3f
vespucchi <56511717+vespucchi@users.noreply.github.com>
2024-05-05 17:49:43 +0200

    Create functions for all the operators

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function add(a, b) { return a + b };
+function subtract(a, b) { return a - b };
+function multiply(a, b) { return a * b };
+function divide(a, b) { return a / b };
+function modulo(a, b) { return a % b };
+function oneDividedBy(a) { return 1 / a };
+function power(a) { return a ** 2 };
+function squareRoot(a) { return Math.sqrt(a) };
+function changeOperator(a) { return a !== 0 ? a * (-1) : a};
+
 
